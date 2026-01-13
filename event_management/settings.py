ce50e2c7016d76951cbe225e26dad66f6ec69839
Ajok-2022 <ajokkuechajokdeng@gmail.com>
2026-01-13 19:01:18 +0200

    Update settings for Swagger integration and open API access

--- event_management/settings.py
+++ event_management/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
+    'drf_yasg',
     'events',
     'users',
 ]
@@ -78,11 +78,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ],
 }
